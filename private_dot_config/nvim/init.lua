53cc523a2f06e2d37a1968ed01000d029858ab44
Soumik <hey@griim.xyz>
2022-12-17 01:15:13 +0530

    update: nvim temp

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,36 +1,18 @@
+local present, _ = pcall(require, "impatient")
 
-local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-		vim.cmd [[packadd packer.nvim]]
-		return true
-	end
-	return false
-end
-
-local packer_bootstrap = ensure_packer()
-
-local gg = require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'norcalli/nvim-colorizer.lua'
-
-	if packer_bootstrap then
-		require('packer').sync()
-	end
-end)
-
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.laststatus = 3
 vim.opt.termguicolors = true
 
-require 'colorizer'.setup {
-	css = {
-		RGB      = true;         -- #RGB hex codes
-		RRGGBB   = true;         -- #RRGGBB hex codes
-		names    = true;         -- "Name" codes like Blue
-		RRGGBBAA = true;         -- #RRGGBBAA hex codes
-		rgb_fn   = true;         -- CSS rgb() and rgba() functions
-	}
-}
-
-return gg
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if not present or vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+	vim.api.nvim_command "packadd packer.nvim"
+	require "plugins"
+	require("packer").sync()
+end
