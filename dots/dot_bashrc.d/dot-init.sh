178eab387acf1b3a9f0ba639e6329d906f8ccfc3
Soumik <griimick@gmail.com>
2024-03-17 18:13:29 +0000

    feat: add .bash_rc.d dir

---
+++ dots/dot_bashrc.d/dot-init.sh
@@ -0,0 +1,37 @@
+THIS_DIR=$(dirname "$0")
+
+# Colorful logging helper: INFO (green) level
+function info() {
+	echo -e "\e[32m*\e[39m ${*}"
+}
+
+# Colorful logging helper: WARN (orange) level
+function warn() {
+	echo -e "\e[33m*\e[39m ${*}"
+}
+
+# Colorful logging helper: ERROR (red) level
+function error() {
+	echo -e "\e[31m*\e[39m ${*}"
+}
+
+# Colorful logging helper: just a new line
+function nln() {
+	echo ""
+}
+
+# Common utilities
+case "${OSTYPE}" in
+	solaris*) OSNAME="SOLARIS" ;;
+	darwin*)  OSNAME="MACOSX" ;;
+	linux*)   OSNAME="LINUX" ;;
+	bsd*)     OSNAME="BSD" ;;
+	msys*)    OSNAME="WINDOWS" ;;
+	*)        OSNAME="${OSTYPE}" ;;
+esac
+
+# Source all the 'bashrc.d' files
+for BASHRC_D_FILE in `ls ${THIS_DIR}/*.sh`; do
+	source $BASHRC_D_FILE
+done
+
