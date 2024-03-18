b94c405aa5e4042b03ecec4cec8391ee393e9329
Soumik <griimick@gmail.com>
2024-03-18 20:59:51 +0000

    feat: make bash completions work

--- dot_bashrc.d/20-starship.sh
+++ dot_bashrc.d/20-starship.sh
@@ -1,4 +1,9 @@
-# Setup starship
-eval "$(starship init bash)"
-starship preset nerd-font-symbols -o ~/.config/starship.toml
-
+# Setup starship if starship bin exists
+STARSHIP_BIN=$HOME/.local/bin/starship
+if [ -x "$STARSHIP_BIN" ]; then
+	eval "$($STARSHIP_BIN init bash)"
+	starship preset nerd-font-symbols -o ~/.config/starship.toml
+elif [ -x "$(which starship >/dev/null)" ]; then
+	eval "$(starship init bash)"
+	starship preset nerd-font-symbols -o ~/.config/starship.toml
+fi
