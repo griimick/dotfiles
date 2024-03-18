c01e3e10a17997cc0835bc4b043ec3db1cedd434
Soumik <griimick@gmail.com>
2024-03-18 23:20:11 +0000

    feat(nvim): add more lsps

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,15 +1,17 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
 				"stylua",
 				"shellcheck",
 				"shfmt",
 				"bash-language-server",
 				"eslint-lsp",
 				"lua-language-server",
-			},
-		},
+				"luacheck",
+				"css-lsp",
+			})
+		end,
 	},
 }
