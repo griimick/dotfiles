93629050cae0568afefb6e3fa841b379ec765bf8
Soumik <griimick@gmail.com>
2024-03-19 13:26:36 +0000

    feat(nvim): add border to quick terminal bind

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
+
+vim.keymap.set("n", "<C-/>", function()
+	Util.terminal(nil, { border = "rounded" })
+end, { desc = "Term with border" })
